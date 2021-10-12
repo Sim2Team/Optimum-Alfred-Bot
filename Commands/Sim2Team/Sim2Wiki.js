2032ca2ed89ed7cbe540b240bb4b68989e1d15ea
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2021-10-12 04:19:53 +0200

    See desc for more.
    - Update Sim2Wiki command for latest Sim2Wiki changes.
    
    - Get rid of `.S2Structures` and `.S2Research`. The `Sim2Wiki` command replaces both of them, so use that instead.
    
    - Move Sim2Editor and Sim2Wiki to a `Sim2Team` Category.

--- Commands/Sim2Team/Sim2Wiki.js
+++ Commands/Sim2Team/Sim2Wiki.js
@@ -10,54 +10,94 @@ const DefaultURL = "https://sim2team.github.io/wiki/";
 const Content = [
 	/* Guides related. */
 	{
-		"Desc": "The index page of the guides.",
+		"Desc": "The index page of the guides",
 		"Title": "Guides Index",
 		"URL": "guides"
 	},
 	{
-		"Desc": "How to export the Game Boy Advance Soundtracks to the best quality as a WAV, MP3 whatever you like.",
+		"Desc": "How to export the Game Boy Advance Soundtracks to the best quality as a WAV, MP3 whatever you like",
 		"Title": "Unkrawerter GBA",
 		"URL": "guides/unkrawertergba"
 	},
 	{
-		"Desc": "How to dump and restore the Savefile.",
+		"Desc": "How to dump and restore the Savefile",
 		"Title": "Save Dump",
 		"URL": "guides/savedump"
 	},
 
 	/* Research general related. */
 	{
-		"Desc": "The index page of the research.",
+		"Desc": "The index page of the research",
 		"Title": "Research Index",
 		"URL": "research"
 	},
 
 	/* Bustin' Out related. */
 	{
-		"Desc": "Save Information for The Sims Bustin' Out (GBA).",
+		"Desc": "Save Information for The Sims Bustin' Out (GBA)",
 		"Title": "Bustin' Out Saveinfo",
 		"URL": "research/bustinout"
 	},
 
 	/* The Urbz (GBA) related. */
 	{
-		"Desc": "Save Information for The Urbz - Sims in the City (GBA).",
+		"Desc": "Save Information for The Urbz - Sims in the City (GBA)",
 		"Title": "The Urbz (GBA) Saveinfo",
 		"URL": "research/urbzgba"
 	},
 
 	/* The Urbz (NDS) related. */
 	{
-		"Desc": "Save Information for The Urbz - Sims in the City (NDS).",
+		"Desc": "Save Information for The Urbz - Sims in the City (NDS)",
 		"Title": "The Urbz (NDS) Saveinfo",
 		"URL": "research/urbznds"
 	},
 
 	/* The Sims 2 (GBA) related. */
 	{
-		"Desc": "Save Information for The Sims 2 (GBA).",
+		"Desc": "Save Information for The Sims 2 (GBA)",
 		"Title": "The Sims 2 (GBA) Saveinfo",
 		"URL": "research/sims2gba"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Cast Member Research",
+		"Title": "The Sims 2 (GBA) Cast Member",
+		"URL": "research/sims2gba/castmember"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Episode Research",
+		"Title": "The Sims 2 (GBA) Episode",
+		"URL": "research/sims2gba/episode"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | House Item Research",
+		"Title": "The Sims 2 (GBA) House Item",
+		"URL": "research/sims2gba/houseitem"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Item Research",
+		"Title": "The Sims 2 (GBA) Item",
+		"URL": "research/sims2gba/item"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Item Package Research",
+		"Title": "The Sims 2 (GBA) Item Package",
+		"URL": "research/sims2gba/itempackage"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Meta Research",
+		"Title": "The Sims 2 (GBA) Meta",
+		"URL": "research/sims2gba/meta"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Meta Header Research",
+		"Title": "The Sims 2 (GBA) Meta Header",
+		"URL": "research/sims2gba/metaheader"
+	},
+	{
+		"Desc": "The Sims 2 Game Boy Advance | Social Move Research",
+		"Title": "The Sims 2 (GBA) Social Move",
+		"URL": "research/sims2gba/socialmove"
 	},
 
 	/* The Sims 2 (NDS) related. */
@@ -65,6 +105,11 @@ const Content = [
 		"Desc": "Save Information for The Sims 2 (NDS).",
 		"Title": "The Sims 2 (NDS) Saveinfo",
 		"URL": "research/sims2nds"
+	},
+	{
+		"Desc": "The Sims 2 Nintendo DS | Painting Research",
+		"Title": "The Sims 2 (NDS) Painting",
+		"URL": "research/sims2nds/painting"
 	}
 ];
 
--- README.md
+++ README.md
@@ -9,25 +9,27 @@ Optimum-Alfred-Bot has the following features as of the latest commit:
 
 ### General Commands
 
-**Commands related to nothing.**
+**Commands related to general things.**
 
 - [x] `About` - Shows some information about the bot.
 - [x] `Help [category] [command]` - Shows a list of categories, commands, or info about a command.
 - [x] `JavaScript <JavaScript Code>` / `JS <JavaScript Code>` - Runs JavaScript code. Only usable for developers.
 - [x] `Reload <Command>` - Reloads a command. Only usable for developers.
 
+### Sim2Team commands
+
 **Commands related to Sim2Team.**
 
+- [x] `Sim2Editor [command]` / `S2Editor [command]` - Some commands related to [Sim2Editor](https://sim2team.github.io/sim2editor/).
 - [x] `Sim2Wiki [content]` / `S2Wiki [content]` - Links to some things from the Sim2Team wiki site. For the best results, search by the actual site path starting at [https://sim2team.github.io/wiki/](https://sim2team.github.io/wiki/) instead of keywords.
 
 ### The Sims 2 Commands
 
 **Commands related to The Sims 2.**
 
-- [x] `Sim2Editor [command]` / `S2Editor [command]` - Some commands related to [Sim2Editor](https://sim2team.github.io/sim2editor/).
 - [x] `S2CastList [command]` / `S2CastMember [command]` - Displays information about the cast members from The Sims 2 Game Boy Advance and Nintendo DS.
-- [x] `S2Research [command]` - Links to some research related things of the Sim2Research Repository.
-- [x] `S2Structures [command]` / `S2Struct [command]` - Links to some structure related things of the Sim2Research Repository.
+- [] `S2Research [command]` - Links to some research related things of the Sim2Research Repository. (This command has been deleted. Use `.Sim2Wiki` for this command instead).
+- [] `S2Structures [command]` / `S2Struct [command]` - Links to some structure related things of the Sim2Research Repository. (This command has been deleted. Use `.Sim2Wiki` for this command instead).
 
 
 ## Credits
